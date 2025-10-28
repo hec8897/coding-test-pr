e4826f402f250b90a2e4d3b4c573e21b6ecd45a0
dawoonkim <hec8897@naver.com>
2025-10-28 19:46:34 +0900

    feat: 폰켓몬 문제 추가

--- programmers/lv1_완주하지_못한_선수/복습.ts
+++ programmers/lv1_완주하지_못한_선수/복습.ts
@@ -1,6 +1,6 @@
 function solution(participant: string[], completion: string[]): string {
-    // 여기에 코드를 작성해주세요!
-    // 힌트: Map을 사용하여 각 참가자의 등장 횟수를 세어보세요
-    
-    return "";
+  // 여기에 코드를 작성해주세요!
+  // 힌트: Map을 사용하여 각 참가자의 등장 횟수를 세어보세요
+
+  return "";
 }
---
+++ programmers/lv1_폰켓몬/solution.ts
@@ -0,0 +1,4 @@
+function solution(nums: number[]): number {
+    // 여기에 코드를 작성해주세요!
+    return 0;
+}
