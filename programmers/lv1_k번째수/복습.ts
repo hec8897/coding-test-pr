47bf026b1f0724b04647140004298e97726237a6
dawoonkim <hec8897@naver.com>
2025-10-28 19:38:34 +0900

    feat: Day 1 문제 복습 파일 추가

---
+++ programmers/lv1_k번째수/복습.ts
@@ -0,0 +1,8 @@
+function solution(array: number[], commands: number[][]): number[] {
+    // 여기에 코드를 작성해주세요!
+    // 힌트: slice()와 sort()를 활용해보세요
+    // commands의 각 요소 [i, j, k]에 대해
+    // array의 i번째부터 j번째까지 자르고 정렬한 후 k번째 수를 구합니다
+    
+    return [];
+}
---
+++ programmers/lv1_모의고사/복습.ts
@@ -0,0 +1,7 @@
+function solution(answers: number[]): number[] {
+    // 여기에 코드를 작성해주세요!
+    // 힌트: 각 수포자의 찍기 패턴을 배열로 만들고
+    // 나머지 연산자(%)를 사용하여 패턴을 반복해보세요
+    
+    return [];
+}
