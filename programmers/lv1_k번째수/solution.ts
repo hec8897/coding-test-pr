bc1c83d780a328a828200363586e946e68a1207e
dawoonkim <hec8897@naver.com>
2025-10-28 18:31:42 +0900

    feat: K번째수 문제 풀이 추가
    - slice, sort, map 메서드 활용
    - 메서드 체이닝으로 코드 간결화
    - 구조 분해 할당 활용
    - 시간복잡도 분석
    - 개선 가능한 부분 정리

---
+++ programmers/lv1_k번째수/solution.ts
@@ -0,0 +1,23 @@
+function solution(array: number[], commands: number[][]): number[] {
+  // 여기에 코드를 작성해주세요!
+  const answer = [];
+
+  for (const [i, j, k] of commands) {
+    answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+  }
+
+  return answer;
+}
+
+// 테스트
+console.log(
+  solution(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3],
+    ]
+  )
+);
+// 예상 결과: [5, 6, 3]
