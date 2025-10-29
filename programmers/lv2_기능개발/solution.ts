894f582390975f4473ff2894e4fd84a35281799d
dawoonkim <hec8897@naver.com>
2025-10-29 11:24:12 +0900

    feat: 기능개발 문제 완료 (큐, 그룹화)

--- programmers/lv2_기능개발/solution.ts
+++ programmers/lv2_기능개발/solution.ts
@@ -1,6 +1,28 @@
 function solution(progresses: number[], speeds: number[]): number[] {
   // 여기에 코드를 작성해주세요!
-  return [];
+  const days = []; // 완료 가능일
+  for (let i = 0; i < progresses.length; i++) {
+    const ramainingDays = Math.ceil((100 - progresses[i]) / speeds[i]);
+    days.push(ramainingDays);
+  }
+
+  const answer: number[] = [];
+  let count = 1;
+  let currentDay = days[0];
+
+  for (let i = 1; i < days.length; i++) {
+    if (days[i] <= currentDay) {
+      count++;
+    } else {
+      answer.push(count);
+      currentDay = days[i];
+      count = 1;
+    }
+  }
+
+  answer.push(count);
+
+  return answer;
 }
 
 // 테스트
